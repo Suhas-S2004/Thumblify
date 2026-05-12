5b25f8b93c1a81205ebcb1ad5c737746dd916bf1
Suhas-S2004 <suhas0168@gmail.com>
2026-05-12 15:37:19 +0530

    session and cookies properties updated

--- server/server.ts
+++ server/server.ts
@@ -30,6 +30,8 @@ app.use(cors({
     origin:['http://localhost:5173','http://localhost:3000',"https://thumblify-client-orcin.vercel.app"],
     credentials: true
 }))
+
+app.set('trust proxy', 1) // trust first proxy
 app.use(session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
@@ -38,7 +40,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'none',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     path: '/'
 },
     store: MongoStore.create({
